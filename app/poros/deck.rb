a1fc72fe94df9a784f8efdd9cb13dbbbac68722a
Nick Edwin <nickedwin85@gmail.com>
2021-05-08 10:53:34 -0600

    Creates a new deck object

---
+++ app/poros/deck.rb
@@ -0,0 +1,19 @@
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    @ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+    @suits = %w(Clubs Diamonds Hearts Spades)
+
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(rank, suit)
+      end
+    end
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
+end
